a006ae6911b4e39185e990b60d5894a21ca23baf
Alan Palazzolo <github@zzolo.org>
2016-03-04 17:21:31 -0600

    Attemption a model guess.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 module.exports = require("./lib/stream-to-sql.js");
+
+
+// var a = new module.exports({
+//   input: "./tests/data/nyc-water-quality-complaints.json",
+//   inputType: "json",
+//   inputOptions: "*"
+// });
+
+var a = new module.exports({
+  input: "./tests/data/nyc-water-quality-complaints.csv"
+});
