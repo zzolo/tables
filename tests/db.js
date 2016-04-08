5bbb409e783150f7786894a3f644adf14b18cba5
Alan Palazzolo <github@zzolo.org>
2016-04-08 11:59:50 -0500

    Adding placeholder for db tests.

---
+++ tests/db.js
@@ -0,0 +1,15 @@
+/* global describe, it, before, beforeEach, after, afterEach */
+
+// Dependencies
+var assert = require("assert");
+var db = require("../lib/db.js");
+
+// DB tests
+describe("db", function() {
+  // TODO: These tests require some more complex setup
+  describe("PLACEHOLDER", function() {
+    it("should write some tests", function() {
+      assert.ok(true);
+    });
+  });
+});
